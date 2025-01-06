922271f5c1f8a4e24c3978a276047dd360189047
VanitasCaesar1 <Chakravarthi8a2ss@gmail.com>
2025-01-06 00:47:19 -0800

    more insights

--- test/main.go
+++ test/main.go
@@ -10,8 +10,18 @@ func main() {
 	var sum_of_1and2 = number1 + number2
 	fmt.Println(sum_of_1and2)
 
+	// you can declare vars like this too
 	name := "chakravrthi"
 	age := "33"
+	//or
+	var name2 = "hello"
+	fmt.Println(name2 + "my name is " + name + " and my age is " + age + "")
 
-	fmt.Println("my name is " + name + " and my age is " + age + "")
+	// u need type formating in here like c lang and using var for clarity
+	var num1 uint = 22
+	var num2 = 35.7
+
+	sum := uint(num2) + num1
+
+	fmt.Println(sum)
 }
